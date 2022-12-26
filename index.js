06d10c50bc6874d17ea847d878f5d147cc23998e
Lina María Salguero <lmsalgueromd@gmail.com>
2022-12-26 03:13:30 -0500

    Complete challenge
    Everything is working

--- index.js
+++ index.js
@@ -3,19 +3,18 @@ let paletteOption = document.getElementById("palette-option")
 let getButton = document.getElementById("get-scheme-button")
 let colorsSection = document.getElementById("colors-section")
 let colorChoser = document.getElementById("color-choser")
-
-/*Main code*/
+    /*Main code*/
 renderSelector()
 getButton.addEventListener("click", renderColors)
 
 /*Functions*/
 function renderSelector() {
-    paletteOption.innerHTML = `<option value="mono">Monochrome</option>
-<option value="mono-dark">Monochrome-dark</option>
-<option value="mono-light">Monochrome-light</option>
+    paletteOption.innerHTML = `<option value="monochrome">Monochrome</option>
+<option value="monochrome-dark">Monochrome-dark</option>
+<option value="monochrome-light">Monochrome-light</option>
 <option value="analogic">Analogic</option>
 <option value="complement">Complement</option>
-<option value="analogic-comp">Analogic-complement</option>
+<option value="analogic-complement">Analogic-complement</option>
 <option value="triad">Triad</option>`
 }
 
@@ -36,11 +35,11 @@ function renderColors() {
 </div>
 
 <div id="color-bar-names" class="color-bar-names">
-    <p id="first-color-name">${firstColor}</p>
-    <p id="second-color-name"></p>
-    <p id="third-color-name"></p>
-    <p id="fourth-color-name"></p>
-    <p id="fifth-color-name"></p>
+    <p id="first-color-name" class="color-name">${firstColor}</p>
+    <p id="second-color-name" class="color-name"></p>
+    <p id="third-color-name" class="color-name"></p>
+    <p id="fourth-color-name" class="color-name"></p>
+    <p id="fifth-color-name" class="color-name"></p>
 </div>`
     let firstColorBar = document.getElementById("first-color")
     let secondColorBar = document.getElementById("second-color")
@@ -52,7 +51,7 @@ function renderColors() {
 
     let schemeArray = []
 
-    fetch(`https://www.thecolorapi.com/scheme?hex=${firstColor.substring(1, 7)}&format=json&mode=analogic&count=4`)
+    fetch(`https://www.thecolorapi.com/scheme?hex=${firstColor.substring(1, 7)}&format=json&mode=${paletteOption.value}&count=4`)
         .then(res => res.json())
         .then(data => {
             schemeArray = data.colors
@@ -78,4 +77,22 @@ function renderColors() {
 
             }
         })
+
+    console.log(paletteOption.value)
+    let colorBarNames = document.getElementById("color-bar-names")
+
+    colorBarNames.addEventListener("click", function() {
+        let colorsArray = []
+        const cb = navigator.clipboard;
+        let paragraph = " "
+        colorsArray = document.getElementsByClassName("color-name");
+        for (let j = 0; j < colorsArray.length; j++) {
+            paragraph += `${colorsArray[j].textContent} `
+        }
+        console.log(paragraph)
+        cb.writeText(paragraph).then(() => alert(`Text copied. The colors you've copy are: ${paragraph}`));
+
+
+    })
+
 }
